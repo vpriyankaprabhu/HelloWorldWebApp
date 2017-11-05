f261bca333fcf2c147d284a8d5b33e1de3ace830
priyanka_p_prabhu <priyanka.p.prabhu@oracle.com>
2017-11-05 22:28:43 +0530

    added script for starting the weblogic server

---
+++ resources/deploy-webapp-start-server.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DOMAIN_HOME=/u01/oracle/domains/base_domain
+
+########### SIGTERM handler ############
+function _term() {
+   echo "Stopping container."
+   echo "SIGTERM received, shutting down the server!"
+   ${DOMAIN_HOME}/bin/stopWebLogic.sh
+}
+########### SIGKILL handler ############
+function _kill() {
+   echo "SIGKILL received, shutting down the server!"
+   kill -9 $childPID
+}
+# Set SIGTERM handler
+trap _term SIGTERM
+# Set SIGKILL handler
+trap _kill SIGKILL
+
+DOMAIN_HOME=/u01/oracle/domains/base_domain
+
+#copying the ear of helloworld web application to autodeploy location of domain home
+cp -rf /u01/oracle/resources/HelloWorldEAR.ear ${DOMAIN_HOME}/autodeploy
+
+# Start Admin Server and tail the logs
+echo "Starting WebLogic..."
+${DOMAIN_HOME}/startWebLogic.sh &
+childPID=$!
+wait $childPID
