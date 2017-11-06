421179e3e8d674fb81f8fa513eabab2f48f4e7b1
priyanka_p_prabhu <priyanka.p.prabhu@oracle.com>
2017-11-06 14:43:32 +0530

    updated scripts for deploying of the applicaton and starting the application

--- resources/deploy-webapp.py
+++ resources/deploy-webapp.py
@@ -6,7 +6,7 @@ adminPort       =   os.environ.get('ADMIN_PORT', '7001')
 adminUser       =   os.environ.get('ADMIN_USERNAME', 'weblogic')
 adminPassword   =   os.environ.get('ADMIN_PASSWORD', 'welcome1')
 print ''
-print 'Host IP Address is :' %socket.gethostbyname(socket.gethostname())
+print 'Host IP Address is :'+socket.gethostbyname(socket.gethostname())
 adminServerUrl  =   "t3://" + socket.gethostbyname(socket.gethostname()) + ":" + adminPort
 
 domainName     =   "base_domain"
@@ -15,7 +15,7 @@ domainHome     =   "/u01/oracle/user_projects/domains/" + domainName
 #Start the AdminServer so that we can deploy the application
 startServer(adminServerName, domainName, adminServerUrl, adminUser, adminPassword, domainHome, block="true")
 print ''
-print 'STARTED %s' %adminServerName
+print 'STARTED %s'+adminServerName
 
 print ''
 print 'CONNECT TO ADMIN SERVER';
