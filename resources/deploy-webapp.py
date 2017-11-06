ec67fbd29fd5d0dbdbefccb2adb0d1ce018da1b5
priyanka_p_prabhu <priyanka.p.prabhu@oracle.com>
2017-11-06 14:37:38 +0530

    updated scripts for deploying of the applicaton and starting the application

--- resources/deploy-webapp.py
+++ resources/deploy-webapp.py
@@ -5,7 +5,8 @@ adminServerName =   "AdminServer"
 adminPort       =   os.environ.get('ADMIN_PORT', '7001')
 adminUser       =   os.environ.get('ADMIN_USERNAME', 'weblogic')
 adminPassword   =   os.environ.get('ADMIN_PASSWORD', 'welcome1')
-print socket.gethostbyname(socket.gethostname())
+print ''
+print 'Host IP Address is :' %socket.gethostbyname(socket.gethostname())
 adminServerUrl  =   "t3://" + socket.gethostbyname(socket.gethostname()) + ":" + adminPort
 
 domainName     =   "base_domain"
@@ -13,20 +14,24 @@ domainHome     =   "/u01/oracle/user_projects/domains/" + domainName
 
 #Start the AdminServer so that we can deploy the application
 startServer(adminServerName, domainName, adminServerUrl, adminUser, adminPassword, domainHome, block="true")
-print 'STARTED %s' % adminServerName
+print ''
+print 'STARTED %s' %adminServerName
 
+print ''
 print 'CONNECT TO ADMIN SERVER';
 connect(adminUser, adminPassword, adminServerUrl);
 
+print ''
 print 'DEPLOYING ...'
 deploy('HelloWorldEAR', '/u01/oracle/HelloWorldEAR.ear', targets='AdminServer')
-
 print '..DEPLOYED WEB APPLICATION.'
+
+print ''
 print 'NOW STARTING APPLICATION...'
 startApplication('HelloWorldEAR')
-
 print '...STARTED APLLICATION HelloWorldEAR'
 
+print ''
 disconnect()
 shutdown(force='true')
 
