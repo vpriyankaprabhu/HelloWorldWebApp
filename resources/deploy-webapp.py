a99ef1b195c1a5928baabd8e7051396393482027
priyanka_p_prabhu <priyanka.p.prabhu@oracle.com>
2017-11-07 11:48:07 +0530

    Reformatted and ReIndented the wercker.yml file, changed permission for shell script

--- resources/deploy-webapp.py
+++ resources/deploy-webapp.py
@@ -24,7 +24,7 @@ connect(adminUser, adminPassword, adminServerUrl);
 
 print ''
 print 'DEPLOYING WEB APPLICATION...'
-deploy('HelloWorldEAR', '/u01/oracle/HelloWorld/HelloWorldEAR.ear', targets='AdminServer')
+deploy('HelloWorldEAR', '/u01/oracle/HelloWorld/resources/HelloWorldEAR.ear', targets='AdminServer')
 print '..DEPLOYED WEB APPLICATION.'
 
 print ''
